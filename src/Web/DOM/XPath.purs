23b64ec68b9908c00e89fd953a995b223e72f465
Brandon Elam Barker <beb82@cornell.edu>
2019-04-18 21:29:38 +0000

    working on ns resolver for metajelo

--- src/Web/DOM/XPath.purs
+++ src/Web/DOM/XPath.purs
@@ -3,12 +3,13 @@ module Web.DOM.Document.XPath where
 import Prelude
 
 import Data.Int                               (round, toNumber)
-import Data.Maybe                             (Maybe{-(..)-})
+import Data.Maybe                             (Maybe(..))
 import Data.Nullable                          (Nullable, toMaybe, toNullable)
 import Data.Natural                           (Natural, intToNat, natToInt)
 import Effect                                 (Effect)
-import Web.DOM.Document                       (Document{-, documentElement-})
-import Web.DOM.Node                           (Node{-, ownerDocument-})
+import Web.DOM.Document                       (Document, documentElement, fromNode)
+import Web.DOM.Element                        (toNode)
+import Web.DOM.Node                           (Node, ownerDocument)
 import Web.DOM.Document.XPath.ResultType      (ResultType)
 
 foreign import data XPathEvaluator :: Type
@@ -76,8 +77,12 @@ foreign import createNSResolver :: Node -> Document -> NSResolver
 
 -- | Same interface as `createNSResolver`, but will use the owner
 -- document as the nodeResolver if it exists. See [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_using_XPath_in_JavaScript#Implementing_a_Default_Namespace_Resolver).
--- TODO: need to add Effect handling here:
--- defaultNSResolver :: Node -> Document -> NSResolver
--- defaultNSResolver nodeRes doc = case ownerDocument nodeRes of
---   Nothing -> documentElement nodeRes
---   Just ownerDoc -> documentElement ownerDoc
+defaultNSResolver :: Node -> Document -> Effect NSResolver
+defaultNSResolver nodeRes doc = do
+  ownerDoc <- ownerDocument nodeRes
+  nodeResFinal <- case ownerDoc of
+    Nothing -> case (fromNode nodeRes) of
+      Nothing -> pure nodeRes
+      Just nodeResEl ->  documentElement (toNode nodeResEl)
+    Just od -> documentElement od
+  createNSResolver nodeResFinal doc
--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Data.Int                          (toNumber)
 import Data.Maybe                        (Maybe(..))
 import Data.Natural                      (intToNat)
+import Debug.Trace                       (traceM)
 import Effect                            (Effect)
 import Effect.Aff                        (Aff)
 import Effect.Class                      (liftEffect)
@@ -31,10 +32,16 @@ parseCatalogDoc dp = parseXMLFromString TD.cdCatalogXml dp
 parseNoteDoc :: DOMParser -> Effect Document
 parseNoteDoc dp = parseXMLFromString TD.noteXml dp
 
+parseMetajeloDoc :: DOMParser -> Effect Document
+parseMetajeloDoc dp = parseXMLFromString TD.metajeloXml dp
 
 atomResolver :: NSResolver
 atomResolver = XP.customNSResolver dummyAtomRes
   where dummyAtomRes _ = "http://www.w3.org/2005/Atom"
+
+metajeloResolver :: NSResolver
+metajeloResolver = XP.customNSResolver dummMJRes
+  where dummMJRes _ = "http://ourdomain.cornell.edu/reuse/v.01"
 
 main :: Effect Unit
 main = runTest do
@@ -53,28 +60,28 @@ main = runTest do
       tlog $ "got a node: " <> (nodeName note)
 
       noteToRes <- liftEffect $ XP.evaluate
-        ("/note/to") note Nothing RT.string_type Nothing noteDoc
+        "/note/to" note Nothing RT.string_type Nothing noteDoc
       noteTo <- liftEffect $ XP.stringValue noteToRes
       tlog $ "got a note to: " <> noteTo
       Assert.equal RT.string_type (XP.resultType noteToRes)
       Assert.equal "Tove" noteTo
 
       cdPriceRes <- liftEffect $ XP.evaluate
-        ("/CATALOG/CD[2]/PRICE") catalog Nothing RT.number_type Nothing catalogDoc
+        "/CATALOG/CD[2]/PRICE" catalog Nothing RT.number_type Nothing catalogDoc
       cdPrice <- liftEffect $ XP.numberValue cdPriceRes
       tlog $ "got a cd price: " <> (show cdPrice)
       Assert.equal RT.number_type (XP.resultType cdPriceRes)
       Assert.equal 9.90 cdPrice
 
       cdYearRes <- liftEffect $ XP.evaluate
-        ("/CATALOG/CD[2]/YEAR") catalog Nothing RT.number_type Nothing catalogDoc
+        "/CATALOG/CD[2]/YEAR" catalog Nothing RT.number_type Nothing catalogDoc
       cdYear <- liftEffect $ XP.numberValue cdYearRes
       tlog $ "got a cd year: " <> (show cdYear)
       Assert.equal RT.number_type (XP.resultType cdYearRes)
       Assert.equal (toNumber 1988) cdYear
 
       cdsSnapRes <- liftEffect $ XP.evaluate
-        ("/CATALOG/CD")
+        "/CATALOG/CD"
         catalog
         Nothing
         RT.unordered_node_snapshot_type
@@ -92,7 +99,7 @@ main = runTest do
       atomFeed <- pure $ toNode atomFeedDoc
 
       atomEntriesRes <- liftEffect $ XP.evaluate
-        ("//dummyns:entry")
+        "//dummyns:entry"
         atomFeed
         (Just atomResolver)
         RT.unordered_node_snapshot_type
@@ -102,6 +109,24 @@ main = runTest do
       tlog $ "got " <> (show atomEntriesLen) <> " atom entries"
       Assert.equal (intToNat 3) atomEntriesLen
 
+    test "metajelo.xml" do
+      domParser <- liftEffect $ makeDOMParser
+
+      metajeloDoc <-liftEffect $ parseMetajeloDoc domParser
+      metajelo <- pure $ toNode metajeloDoc
+
+      metajeloIdRes <- liftEffect $ XP.evaluate
+        "/record/identifier"
+        metajelo
+        (Just metajeloResolver)
+        RT.string_type
+        Nothing
+        metajeloDoc
+      traceM metajeloIdRes -- DEBUG
+      metajeloId <- liftEffect $ XP.stringValue metajeloIdRes
+      tlog $ "got metajelo id" <> metajeloId
+      Assert.equal RT.string_type (XP.resultType metajeloIdRes)
+      Assert.equal "OjlTjf" metajeloId
+
 tlog :: forall a. Show a => a -> Aff Unit
 tlog = liftEffect <<< logShow
-
