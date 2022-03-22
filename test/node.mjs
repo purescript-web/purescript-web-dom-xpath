c56bca0c7e5b41d407ef27847444a93e912a8c6e
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 16:40:06 -0500

    Fix Test.Main import

--- test/node.mjs
+++ test/node.mjs
@@ -1,7 +1,7 @@
 import {JSDOM} from "jsdom";
-import Main from "../output/Test.Main/index.js";
+import { main } from "../output/Test.Main/index.js";
 const { XPathResult, DOMParser } = new JSDOM().window;
 
 Object.assign(global, { XPathResult, DOMParser });
 
-Main.main({ browser: false })();
+main({ browser: false })();
