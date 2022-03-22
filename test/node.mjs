34c65a9bccfc7236e19a89236f5ce2404f017dbf
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 18:17:53 -0500

    Revert import order back to original

--- test/node.mjs
+++ test/node.mjs
@@ -1,7 +1,8 @@
 import {JSDOM} from "jsdom";
-import { main } from "../output/Test.Main/index.js";
 const { XPathResult, DOMParser } = new JSDOM().window;
 
 Object.assign(global, { XPathResult, DOMParser });
+
+import { main } from "../output/Test.Main/index.js";
 
 main({ browser: false })();
