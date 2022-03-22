76961405592c0bad09b41c479eaa2eb8a3fd5041
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 15:04:43 -0500

    Replaced 'export var' with 'export const'

--- src/Web/DOM/XPath/ResultType.js
+++ src/Web/DOM/XPath/ResultType.js
@@ -1,13 +1,13 @@
 "use strict";
 
-export var any_type = XPathResult.ANY_TYPE;
-export var number_type = XPathResult.NUMBER_TYPE;
-export var string_type = XPathResult.STRING_TYPE;
-export var boolean_type = XPathResult.BOOLEAN_TYPE;
-export var unordered_node_iterator_type = XPathResult.UNORDERED_NODE_ITERATOR_TYPE;
-export var ordered_node_iterator_type = XPathResult.ORDERED_NODE_ITERATOR_TYPE;
-export var unordered_node_snapshot_type = XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE;
-export var ordered_node_snapshot_type = XPathResult.ORDERED_NODE_SNAPSHOT_TYPE;
-export var any_unordered_node_type = XPathResult.ANY_UNORDERED_NODE_TYPE;
-export var first_ordered_node_type = XPathResult.FIRST_ORDERED_NODE_TYPE;
+export const any_type = XPathResult.ANY_TYPE;
+export const number_type = XPathResult.NUMBER_TYPE;
+export const string_type = XPathResult.STRING_TYPE;
+export const boolean_type = XPathResult.BOOLEAN_TYPE;
+export const unordered_node_iterator_type = XPathResult.UNORDERED_NODE_ITERATOR_TYPE;
+export const ordered_node_iterator_type = XPathResult.ORDERED_NODE_ITERATOR_TYPE;
+export const unordered_node_snapshot_type = XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE;
+export const ordered_node_snapshot_type = XPathResult.ORDERED_NODE_SNAPSHOT_TYPE;
+export const any_unordered_node_type = XPathResult.ANY_UNORDERED_NODE_TYPE;
+export const first_ordered_node_type = XPathResult.FIRST_ORDERED_NODE_TYPE;
 
